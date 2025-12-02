29e1156670afa0cdfbf4136bd2dc24aa425da57b
Cronos962 <sanskarrajput962@gmail.com>
2025-12-02 18:05:26 +0530

    Centered the outlet vertically and horizontally

--- ecomm-user-client/src/components/layout/Layout.jsx
+++ ecomm-user-client/src/components/layout/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = () => {
   return (
     <div className='min-h-screen flex flex-col'>
       <Navbar/>
-      <main className='grow'>
+      <main className='flex grow mx-auto items-center'>
         <Outlet/>        
       </main>
       <Footer/>
