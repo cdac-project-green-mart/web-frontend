b1c60514c8335dcf86c1c6c8e636fdcea84be354
Cronos962 <sanskarrajput962@gmail.com>
2025-12-07 11:54:44 +0530

    Added Popular Categories section with UI, assets and layout integration

--- ecomm-user-client/src/pages/Home.jsx
+++ ecomm-user-client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import HeroSection from "../components/heroSection/HeroSection";
+import PopularCategories from "../components/popularCategories/PopularCategories";
 
 export default function Home() {
   return (
    <div className="px-4 md:px-10">
       <HeroSection/>
+      <PopularCategories/>
       {/* other sections will come here */}
     </div>
   );
