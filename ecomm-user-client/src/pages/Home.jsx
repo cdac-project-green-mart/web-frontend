d6983972bbc511de5ae957d055a338d9876eb970
Cronos962 <sanskarrajput962@gmail.com>
2025-12-07 11:34:53 +0530

    Integrated hero section layout and banner image

--- ecomm-user-client/src/pages/Home.jsx
+++ ecomm-user-client/src/pages/Home.jsx
@@ -1,7 +1,10 @@
+import HeroSection from "../components/heroSection/HeroSection";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+   <div className="px-4 md:px-10">
+      <HeroSection/>
+      {/* other sections will come here */}
     </div>
   );
 }
