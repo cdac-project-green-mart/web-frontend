d1386090760a512ddf0577aa2a33077be48706b8
Shubh.git <shubhjain07112@gmail.com>
2025-12-10 14:30:12 +0530

    Add About page

---
+++ ecomm-user-client/src/pages/About.jsx
@@ -0,0 +1,18 @@
+import AboutIntro from "../components/about/AboutIntro";
+import AboutFeatures from "../components/about/AboutFeatures";
+import AboutDelivery from "../components/about/AboutDelivery";
+import Testimonials from "../components/testimonials/Testimonials";
+export default function About(){
+    return (
+        <div>
+    <AboutIntro/>
+    <AboutFeatures/>
+    <AboutDelivery/>
+    <div className="container h-[400px] flex items-center justify-center bg-[#F2F2F2]">
+    <Testimonials/>
+    </div>
+    <div className="bg-white"> <br></br></div>
+    </div>
+    );
+
+}
