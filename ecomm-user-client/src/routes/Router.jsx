b0489727c5914378e0246afd48e7df66ac8e344b
Shubh.git <shubhjain07112@gmail.com>
2026-02-03 02:13:15 +0530

    Added routes of orders.jsx

--- ecomm-user-client/src/routes/Router.jsx
+++ ecomm-user-client/src/routes/Router.jsx
@@ -6,9 +6,12 @@ import Login from '../pages/Login.jsx'
 import Register from '../pages/Register.jsx'
 import Checkout from '../pages/Checkout.jsx'
 import Orders from '../pages/Orders.jsx'
+import OrderDetails from '../pages/OrderDetails.jsx'
+
 import Layout from '../components/layout/Layout.jsx'
 import NotFound from '../pages/NotFound.jsx'
 import About from '../pages/About.jsx'
+
 import ProductDetails from '../pages/ProductDetails.jsx';
 export default function Router() {
   return (
@@ -20,9 +23,12 @@ export default function Router() {
         <Route path="cart" element={<Cart />} />
         <Route path="checkout" element={<Checkout />} />
         <Route path="orders" element={<Orders />} />
+        <Route path="orders/:orderId" element={<OrderDetails />} />
+      
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
         <Route path="about" element={<About />} />
+       
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
